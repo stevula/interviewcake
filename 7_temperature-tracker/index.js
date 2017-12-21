a92e2c69cfaba7844b9dcfbb08c0cae095936801
stevula <brodericksteven@gmail.com>
2017-12-21 14:47:34 -0800

    Finish problem 7

--- 7_temperature-tracker/index.js
+++ 7_temperature-tracker/index.js
@@ -1,3 +1,49 @@
 exports.default = class TempTracker {
+  constructor() {
+    this.temperatures = {};
+    this.entries = 0;
+    this.sum = 0;
+    this.max = null;
+    this.min = null;
+    this.mode = null;
+    this.modeCount = 0;
+  }
 
+  // records a new temperature
+  insert(temperature) {
+    if (this.temperatures[temperature]) {
+      this.temperatures[temperature].occurrences += 1;
+    } else {
+      this.temperatures[temperature] = { occurrences: 1 };
+    }
+    if (this.temperatures[temperature].occurrences > this.modeCount) {
+      this.modeCount = this.temperatures[temperature].occurrences;
+      this.mode = temperature;
+    }
+    if (this.max === null || temperature > this.max) this.max = temperature;
+    if (this.min === null || temperature < this.min) this.min = temperature;
+    this.entries += 1;
+    this.sum += temperature;
+  }
+
+  // returns the highest temp we've seen so far
+  getMax() {
+    return this.max;
+  }
+
+  // returns the lowest temp we've seen so far
+  getMin() {
+    return this.min;
+  }
+
+  // returns the mean of all temps we've seen so far
+  getMean() {
+    // console.log(this.temperatures, this.sum, this.entries)
+    return this.sum / this.entries;
+  }
+
+  // returns a mode of all temps we've seen so far
+  getMode() {
+    return this.mode;
+  }
 }
--- 7_temperature-tracker/test.js
+++ 7_temperature-tracker/test.js
@@ -2,7 +2,48 @@ const expect = require('chai').expect;
 const TempTracker = require('./index').default;
 
 describe('TempTracker', function() {
-  it('', function() {
-    expect().to.equal();
+  let tempTracker;
+  beforeEach(function() {
+    tempTracker = new TempTracker();
+  });
+
+  describe('insert()', function() {
+    it('adds a temperature to the temperatures property', function() {
+      const temp = 100;
+      expect(tempTracker.temperatures).not.to.have.own.property(temp);
+      tempTracker.insert(temp);
+      expect(tempTracker.temperatures).to.have.own.property(temp);
+    });
+  });
+
+  describe('with recorded temperatures', function() {
+    const temperatures = [100, 0, 25, 75, 50, 50];
+    beforeEach(function() {
+      temperatures.forEach(temp => tempTracker.insert(temp));
+    });
+
+    describe('getMax()', function() {
+      it('returns the highest temperature added so far', function() {
+        expect(tempTracker.getMax()).to.equal(Math.max(...temperatures));
+      });
+    });
+
+    describe('getMin()', function() {
+      it('returns the lowest temperature added so far', function() {
+        expect(tempTracker.getMin()).to.equal(Math.min(...temperatures));
+      });
+    });
+
+    describe('getMean()', function() {
+      it('returns the mean of the recorded temperatures', function() {
+        expect(tempTracker.getMean()).to.equal(50);
+      });
+    });
+
+    describe('getMode()', function() {
+      it('returns the mode of the recorded temperatures', function() {
+        expect(tempTracker.getMode()).to.equal(50);
+      });
+    });
   });
 });

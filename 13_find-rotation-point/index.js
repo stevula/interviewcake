2fa6bb6619d52b61364f2a3815b7a449b8cbb997
stevula <brodericksteven@gmail.com>
2017-12-24 18:11:05 -0800

    Solve 13

--- 13_find-rotation-point/index.js
+++ 13_find-rotation-point/index.js
@@ -1,3 +1,15 @@
 exports.default = function findRotationPoint(rotated) {
+  let lowerBound = 0;
+  let upperBound = rotated.length - 1;
+  while (true) {
+    const mid = Math.round((lowerBound + upperBound) / 2);
+    if (rotated[mid] < rotated[mid - 1]) return mid;
 
+    if (rotated[mid] > rotated[lowerBound]) {
+      lowerBound = mid;
+    }
+    else if (rotated[mid] < rotated[lowerBound]) {
+      upperBound = mid;
+    }
+  }
 };
--- 13_find-rotation-point/test.js
+++ 13_find-rotation-point/test.js
@@ -2,21 +2,35 @@ const expect = require('chai').expect;
 const findRotationPoint = require('./index').default;
 
 describe('findRotationPoint', function() {
-  const words = [
-    'ptolemaic',
-    'retrograde',
-    'supplant',
-    'undulate',
-    'xenoepist',
-    'asymptote',  // <-- rotates here!
-    'babka',
-    'banoffee',
-    'engender',
-    'karpatka',
-    'othellolagkage',
-  ];
+  it('finds the index of the rotation point', function() {
+    const words = [
+      'ptolemaic',
+      'retrograde',
+      'supplant',
+      'undulate',
+      'xenoepist',
+      'asymptote',  // <-- rotates here!
+      'babka',
+      'banoffee',
+      'engender',
+      'karpatka',
+      'othellolagkage',
+    ];
+    expect(findRotationPoint(words)).to.equal(words.indexOf('asymptote'));
+  });
 
   it('finds the index of the rotation point', function() {
-    expect(findRotationPoint(words)).to.equal(words.indexOf('asymptote'));
+    const words = [ 'k','v','a','b','c','d','e','g','i' ];
+    expect(findRotationPoint(words)).to.equal(words.indexOf('a'));
+  });
+
+  it('finds the index of the rotation point', function() {
+    const words = [ 'b','c','d','e','g','i','k','v','a' ];
+    expect(findRotationPoint(words)).to.equal(words.indexOf('a'));
+  });
+
+  it('finds the index of the rotation point', function() {
+    const words = ['v','a','b','c','d','e','g','i','k' ];
+    expect(findRotationPoint(words)).to.equal(words.indexOf('a'));
   });
 });

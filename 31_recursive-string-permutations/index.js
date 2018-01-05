0ec348e0623c8bf188b96b8ac02d5b809f3c19be
stevula <brodericksteven@gmail.com>
2018-01-04 22:50:34 -0800

    Improve tests

--- 31_recursive-string-permutations/index.js
+++ 31_recursive-string-permutations/index.js
@@ -1,11 +1,9 @@
 exports.getPermutations = function getPermutations(string) {
-  if (string.length <= 1) {
-    return [string];
-  }
+  if (string.length <= 1) return [string];
 
   const permutations = [];
   const firstPart = string.slice(0, -1);
-  const char = string[string.length - 1];
+  const char = string.slice(-1);
 
   const permutationsOfFirstPart = getPermutations(firstPart);
   permutationsOfFirstPart.forEach((permutationOfFirstPart) => {
--- 31_recursive-string-permutations/test.js
+++ 31_recursive-string-permutations/test.js
@@ -2,15 +2,44 @@ const { expect } = require('chai');
 const { getPermutations } = require('./index');
 
 describe('getPermutations', function() {
-  it('returns all permutations of a string with length 1', function() {
-    expect(getPermutations('a')).to.deep.eq(['a']);
-  });
+  // it('returns all permutations of a string with length 1', function() {
+  //   expect(getPermutations('a')).to.deep.eq(['a']);
+  // });
 
-  it('returns all permutations of a string with length 2', function() {
-    expect(getPermutations('ab').sort()).to.deep.eq(['ab', 'ba'].sort());
-  });
+  // it('returns all permutations of a string with length 2', function() {
+  //   expect(getPermutations('ab').sort()).to.deep.eq(['ab', 'ba'].sort());
+  // });
 
-  it('returns all permutations of a string with length 3', function() {
-    expect(getPermutations('abc').sort()).to.deep.eq(['abc', 'acb', 'cba', 'cab', 'bac', 'bca'].sort());
+  // it('returns all permutations of a string with length 3', function() {
+  //   expect(getPermutations('abc').sort()).to.deep.eq(['abc', 'acb', 'bac', 'bca', 'cab', 'cba']);
+  // });
+
+  it('returns all permutations of a string with length 4', function() {
+    expect(getPermutations('abcd').sort()).to.deep.eq([
+      'abcd',
+      'abdc',
+      'acbd',
+      'acdb',
+      'adbc',
+      'adcb',
+      'bacd',
+      'badc',
+      'bcad',
+      'bcda',
+      'bdac',
+      'bdca',
+      'cabd',
+      'cadb',
+      'cbad',
+      'cbda',
+      'cdab',
+      'cdba',
+      'dabc',
+      'dacb',
+      'dbac',
+      'dbca',
+      'dcab',
+      'dcba',
+    ]);
   });
 });
